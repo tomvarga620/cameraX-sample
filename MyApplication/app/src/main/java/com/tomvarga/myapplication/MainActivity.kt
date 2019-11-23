41415e25162832877dda39c59639ea96169b76de
tomvarga620 <tomas.varga@akademiasovy.sk>
2019-11-23 17:13:42 +0100

    added bottom menu

--- MyApplication/app/src/main/java/com/tomvarga/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/tomvarga/myapplication/MainActivity.kt
@@ -1,138 +1,40 @@
 package com.tomvarga.myapplication
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.graphics.Matrix
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.util.Size
-import android.view.Surface
-import android.view.TextureView
-import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.Toast
-import androidx.camera.core.*
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.lifecycle.LifecycleOwner
-import java.io.File
-import java.util.concurrent.Executors
+import com.google.android.material.bottomnavigation.BottomNavigationMenu
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
+class MainActivity : AppCompatActivity() {
 
-private const val REQUEST_CODE_PERMISSIONS = 10
-private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
-
-class MainActivity : AppCompatActivity(), LifecycleOwner {
+    private lateinit var bottomMenu: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewFinder = findViewById(R.id.view_finder)
+        bottomMenu = findViewById(R.id.bottom_navigation)
 
-        if(allPermissonsGranted()){
-            viewFinder.post { startCamera() }
-        } else {
-            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
-        }
-
-    }
-
-    private val executor = Executors.newSingleThreadExecutor()
-    private lateinit var viewFinder: TextureView
-
-    private fun startCamera(){
-        val previewConfig = PreviewConfig.Builder().apply{
-            setTargetResolution(Size(640,480))
-        }.build()
-
-        val preview = Preview(previewConfig)
-
-        preview.setOnPreviewOutputUpdateListener{
-            val parent = viewFinder.parent as ViewGroup
-            parent.removeView(viewFinder)
-            parent.addView(viewFinder,0)
-
-            viewFinder.surfaceTexture = it.surfaceTexture
-            updateTransfrom()
-        }
-
-
-        val imageCaptureConfig = ImageCaptureConfig.Builder().apply{
-            setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
-        }.build()
-
-        val imageCapture = ImageCapture(imageCaptureConfig)
-
-        findViewById<ImageButton>(R.id.capture_button).setOnClickListener {
-            val file = File(externalMediaDirs.first(),
-                "${System.currentTimeMillis()}.jpg")
-
-            imageCapture.takePicture(file,executor,
-                object : ImageCapture.OnImageSavedListener{
-                    override fun onImageSaved(file: File) {
-                        val msg = "Photo captured - ${file.absolutePath}"
-                        Log.d("CameraX",msg)
-                        viewFinder.post {
-                            Toast.makeText(baseContext, msg ,Toast.LENGTH_SHORT).show()
-                        }
-                    }
-
-                    override fun onError(
-                        imageCaptureError: ImageCapture.ImageCaptureError,
-                        message: String,
-                        cause: Throwable?
-                    ) {
-                        val msg = "Photo capture failed: $message"
-                        Log.e("CameraX", msg, cause)
-                        viewFinder.post {
-                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                })
-        }
-        CameraX.bindToLifecycle(this,preview,imageCapture)
-    }
-
-    private fun updateTransfrom() {
-        val matrix = Matrix()
-
-        val centerX = viewFinder.width/2f
-        val centerY = viewFinder.height/2f
-
-        val rotationDegrees = when(viewFinder.display.rotation){
-            Surface.ROTATION_0 -> 0
-            Surface.ROTATION_90 -> 90
-            Surface.ROTATION_180 -> 180
-            Surface.ROTATION_270 -> 270
-            else -> return
-        }
-        matrix.postRotate(-rotationDegrees.toFloat(), centerX,centerY)
-        viewFinder.setTransform(matrix)
-    }
-
-    private fun updateCamera(){
-
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        if(requestCode == REQUEST_CODE_PERMISSIONS){
-            if(allPermissonsGranted()){
-                viewFinder.post {startCamera()}
-            }else {
-                Toast.makeText(this,"Permission failed",Toast.LENGTH_LONG).show()
-                finish()
+        bottomMenu.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.shoot_action -> {
+                    val intent = Intent(this, ShootActivity::class.java)
+                    startActivity(intent)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.main_action -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.settings_action -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    return@setOnNavigationItemSelectedListener true
+                }
             }
+            true
         }
-    }
-
-    private fun allPermissonsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(
-            baseContext,it) == PackageManager.PERMISSION_GRANTED
     }
 }
